5a8c746a016a98dbf0099e2215ba46b402341d5a
Zan-the-Man01 <55000907+Zan-the-Man01@users.noreply.github.com>
2021-01-03 17:47:06 -0600

    Fix buttons on slideshow

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,38 @@
 var slideIndex = 0;
+var click = false;
+var timer
 showSlides();
+
+// Next/previous controls
+function plusSlides(n) {
+  click = true;
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  click = true;
+  showSlides(slideIndex = n);
+}
 
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  if (!click) {slideIndex++;}
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 6000); // Change image every 2 seconds
+  if (!click) {timer = setTimeout(showSlides, 6000);} // Change image every 2 seconds
+  else {
+    clearTimeout(timer);
+    timer = setTimeout(showSlides, 6000);
+  }
+  dots[slideIndex-1].className += " active";
+  click = false;
 }
